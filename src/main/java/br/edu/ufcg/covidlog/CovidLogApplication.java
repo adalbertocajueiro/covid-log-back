834a746bcdac21d9acf8b0e25270c6ff790bfd87
marcelovitorino <marcelo.vitorino@ccc.ufcg.edu.br>
2020-04-11 12:47:04 -0300

    added cors option

--- src/main/java/br/edu/ufcg/covidlog/CovidLogApplication.java
+++ src/main/java/br/edu/ufcg/covidlog/CovidLogApplication.java
@@ -2,13 +2,31 @@ package br.edu.ufcg.covidlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @RestController
 public class CovidLogApplication {
+	
+	
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
+
 
 	public static void main(String[] args) {
 		System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
