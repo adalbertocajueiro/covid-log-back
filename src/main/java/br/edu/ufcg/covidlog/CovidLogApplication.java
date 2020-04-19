eb722d6e30dbf5212e00541a76875efc9c68d89f
Adalberto Cajueiro <adalberto.cajueiro@gmail.com>
2020-04-19 09:33:55 -0300

    remocao de import nao usado.

--- src/main/java/br/edu/ufcg/covidlog/CovidLogApplication.java
+++ src/main/java/br/edu/ufcg/covidlog/CovidLogApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
